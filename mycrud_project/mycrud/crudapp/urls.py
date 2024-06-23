9fd0b6874593b302d1f9db34b72039e9f7f13292
jaymind2810 <jaymind2810@gmail.com>
2024-06-23 11:28:49 +0530

    [REMOVE] Remove some unneccessory modules.

--- mycrud_project/mycrud/crudapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('add/', views.add_registration, name='add_registration'),
-    path('list/', views.registration_list, name='registration_list'),
-]
